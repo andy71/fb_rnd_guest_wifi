0244266ae4416cc902341fd0ce99cff71e48dc7a
andy71 <andy71@users.noreply.github.com>
2017-12-15 10:32:20 +0100

    Update fb_settings.php

--- fb_settings.php
+++ fb_settings.php
@@ -1,18 +1,18 @@
 <?php
-$fritzboxPort	= "49000";
-$login			= 'Username';
-$password		= 'Password';
+$Default_Port		= "49000";
+$Default_Login		= 'Username';
+$Default_Password	= 'Password';
 
 $box_arr[] = array(
 				"IP"		=> "fritz.box",
-				"Port"		=> $fritzboxPort,
-				"Login"		=> $login,
-				"Password"	=> $password
+				"Port"		=> $Default_Port,
+				"Login"		=> $Default_Login,
+				"Password"	=> $Default_Password
 );
-$box_arr[] = array(
-				"IP"		=> "fritz.powerline",
-				"Port"		=> $fritzboxPort,
-				"Login"		=> $login,
-				"Password"	=> $password
-);
+//$box_arr[] = array(
+//				"IP"		=> "fritz.powerline",
+//				"Port"		=> $Default_Port,
+//				"Login"		=> $Default_Login,
+//				"Password"	=> $Default_Password
+//);
 ?>
